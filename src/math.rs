532efcd31dc63f56a369df8f48f6446139da2532
LawanF <lawanf@users.noreply.github.com>
2024-08-04 20:19:25 +0200

    adjusted sawtooth function to have the same period

--- src/math.rs
+++ src/math.rs
@@ -9,9 +9,9 @@ pub fn triangle(angle: f64) -> f64 {
 }
 
 pub fn square(angle: f64) -> f64 {
-    if sin(angle) >= 0.0 { 1.0 } else { -1.0 }
+    if libm::sin(angle) >= 0.0 { 1.0 } else { -1.0 }
 }
 
-pub fn saw(angle: f64) -> f64 {
-    angle.rem_euclid(2.0) - 1
+pub fn sawtooth(angle: f64) -> f64 {
+    (angle / (2.0 * PI)).rem_euclid(2.0) - 1.0
 }
