bb887364932de10a6ad7c0cc669b0d235de41389
LawanF <lawanf@users.noreply.github.com>
2024-08-04 20:23:26 +0200

    adjusted visibility for wave funcs

--- src/math.rs
+++ src/math.rs
@@ -1,17 +1,17 @@
 use std::f64::consts::PI;
 
-pub fn sin(angle: f64) -> f64 {
+fn sin(angle: f64) -> f64 {
     libm::sin(angle)
 }
 
-pub fn triangle(angle: f64) -> f64 {
+fn triangle(angle: f64) -> f64 {
     libm::asin(libm::sin(angle)) / (PI / 2.0)
 }
 
-pub fn square(angle: f64) -> f64 {
+fn square(angle: f64) -> f64 {
     if libm::sin(angle) >= 0.0 { 1.0 } else { -1.0 }
 }
 
-pub fn sawtooth(angle: f64) -> f64 {
+fn sawtooth(angle: f64) -> f64 {
     (angle / (2.0 * PI)).rem_euclid(2.0) - 1.0
 }
