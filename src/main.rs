2a347d39eb07349fdcd256968cf262611c414840
LawanF <lawanf@users.noreply.github.com>
2024-08-27 21:21:39 +0200

    Implemented key detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use nannou::prelude::*;
+
+const WINDOW_WIDTH: u32 = 500;
+const WINDOW_HEIGHT: u32 = 500;
+
+struct Model {}
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .loop_mode(LoopMode::wait())
+        .run();
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window()
+                    .title(app.exe_name().unwrap())
+                    .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+                    .view(view)
+                    .key_pressed(key_pressed)
+                    .key_released(key_released)
+                    .build()
+                    .unwrap();
+
+    Model {}
+}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+}
+
+fn key_pressed(_app: &App, _model: &mut Model, key: Key) {
+    println!("Pressed: {:?}", key);
+}
+
+fn key_released(_app: &App, _model: &mut Model, key: Key) {
+    println!("Released: {:?}", key);
 }
